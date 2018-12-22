d6975995bdc5153edf98735eb4753ce0df812bd4
unknown <cdog2035@gmail.com>
2018-12-22 23:47:49 +0800

    finish

--- rd.c
+++ rd.c
@@ -6,40 +6,27 @@
 int main(int argc, char *argv[])
 {
 
-
-	int fd = open("UserList", O_RDONLY);
-	//char *targetUser = argv[1];
-	//printf(1,"user: %s\n", targetUser);
-	//char nameList[128];
-	//char *names = nameList;
-	char buf[1024];
+	int fd = open(argv[1],O_RDONLY);
+	char buf[2048];
 	int checkReadFile = read(fd, buf,sizeof buf);
 	close(fd);
 	if(checkReadFile < 0)
 	{
-		printf(1,"Can't find user list\n");
+		printf(1,"Can't open file.\n");
 	}
 	else
 	{
-
-		fd = open("UserList", O_RDWR);
+		
+		/*fd = open("UserList", O_RDWR);
 		if(fd >= 0)
 		{
 			int i = strlen(buf);
 			if (buf[i-1] == '\n')
 				i--;
 			int j = 0;
-			int k = 0;
 			for(j = 0; j < strlen(argv[1]); j++)
 			{
 				buf[i] = argv[1][j];
-				i++;
-			}
-			buf[i]=':';
-			i++;
-			for(k = 0; k < strlen(argv[2]); k++)
-			{
-				buf[i] = argv[2][k];
 				i++;
 			}
 			buf[i]=':';
@@ -47,10 +34,10 @@ int main(int argc, char *argv[])
 			//printf(1,"buf: %s\n", buf);
 			write(fd,buf,strlen(buf)+1);
 			close(fd);
-		}
-		printf(1,"User added\n");
-	}
-	
+		}*/
+		printf(1,buf);
+	}	
+
 	exit();
 
 
